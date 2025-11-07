4f3163fabdca004076eec738995f42d5bb66c779
Alberto-Enti <alberto.sanchez@estudiant.enti.cat>
2025-11-07 16:49:13 +0100

    movement y InputSystem import

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float speedRotation;
+    private Vector3 moveDirection;
+    private Vector2 moveReadings;
+    private Rigidbody rb;
     public Transform target;
+    InputSystem_Actions userInput;
+
+    private void Awake()
+    {
+        userInput = new InputSystem_Actions();
+    }
+    private void OnEnable()
+    {
+        userInput.Enable();
+    }
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
-    {        
+    {
+        moveReadings = userInput.Player.Move.ReadValue<Vector2>();
+        moveDirection.x = moveReadings.x;
+        moveDirection.z = moveReadings.y;
+        moveDirection = moveDirection.normalized;
+    }
 
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveDirection * speed *  Time.fixedDeltaTime;
+    }
+
+    private void OnMove()
+    {
+        Debug.Log("MOVE");
     }
 }
