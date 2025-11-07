c477544517d0bb11deb0672182cfdb99b9297361
Alberto-Enti <alberto.sanchez@estudiant.enti.cat>
2025-11-07 17:12:22 +0100

    Enemy LookAt

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,13 +7,15 @@ public class EnemyController : MonoBehaviour
 {
     public float speedRotation;
     public float stoppingDistance;
+    public Transform target;
+    private Quaternion targetRot;
     private void Start()
     {
-
     }
     private void Update()
     {
-
+        targetRot = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, speedRotation * Time.deltaTime);
     }
 
     public void Kill()
